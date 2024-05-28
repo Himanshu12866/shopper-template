80d27ad6d1f0856a1b1ec30ee8e2353e6b1d0014
Himanshu12866 <manshu010m@gmail.com>
2024-05-28 13:45:43 +0530

    Added Script

---
+++ script.js
@@ -0,0 +1,23 @@
+var CountDownDate = new Date('Apr 22, 2030 12:00:00').getTime();
+var x = setInterval(() => {
+    var now = new Date().getTime()
+    var distance = CountDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    if (distance < 0) {
+        document.getElementById('days').innerHTML = '00';
+        document.getElementById('hours').innerHTML = '00';
+        document.getElementById('minutes').innerHTML = '00';
+        document.getElementById('seconds').innerHTML = '00';
+
+    }
+}, 1000)
